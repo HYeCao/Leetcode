5802e9fcf46df1d6b544b7504d61d6f904765235
caohongye <740061271@qq.com>
2020-06-23 18:47:10 +0800

    6.23

--- src/d_6June/Fraction.java
+++ src/d_6June/Fraction.java
@@ -1,7 +1,18 @@
 package d_6June;
 
+/**
+ * 思路：递归 + 通分
+ */
 public class Fraction {
     public int[] fraction(int[] cont) {
-        return 0;
+
+        return get(cont,0);
+    }
+    public int[] get(int[] cont,int num){
+        if(num==cont.length-1){
+            return new int[]{1*cont[num],1};
+        }
+        int[] nextRes = get(cont, num+1);
+        return new int[]{cont[num] * nextRes[0] + nextRes[1], nextRes[0]};
     }
 }
